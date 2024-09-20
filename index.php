b71ebbf897454e88a7557fef70e0776d42c3d33f
Hans Root <sudo.hroot@gmail.com>
2024-09-20 16:07:05 +0300

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Todo PHP</title>
+</head>
+
+<body class="bg-gray-100">
+  <h1 class="text-3xl">Todo App</h1>
+</body>
+
+</html>
